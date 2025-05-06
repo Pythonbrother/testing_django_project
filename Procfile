b86beca80581d7d2e34b7031e784096e8ca84107
Pythonbrother <167950118+Pythonbrother@users.noreply.github.com>
2025-05-06 11:00:47 +0630

    add required files

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn special_project.wsgi
+web: python manage.py migrate && gunicorn special_project.wsgi
--- special_project/settings.py
+++ special_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-udip3ijuu9s6ief9n8$c15f6s$ny7k#+qm89-$u8ns2s)bcs)t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
