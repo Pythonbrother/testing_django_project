007de0a3d4c18315a65f0be83e219f982ad7c661
Pythonbrother <167950118+Pythonbrother@users.noreply.github.com>
2025-07-27 14:14:38 +0630

    change in css

--- special_project/settings.py
+++ special_project/settings.py
@@ -132,12 +132,12 @@ NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-#STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static')]
+#STATIC_URL = "static/"
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
